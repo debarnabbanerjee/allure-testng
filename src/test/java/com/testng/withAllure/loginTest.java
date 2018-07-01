fe0945b764687542484cb107c0e31a12ddb230b6
Debarnab Banerjee <banerjee.debarnab@gmail.com>
2018-07-01 10:26:28 +0200

    adding the capabily to add api response, header, code in allure report

--- src/test/java/com/testng/withAllure/loginTest.java
+++ src/test/java/com/testng/withAllure/loginTest.java
@@ -1,20 +1,19 @@
 package com.testng.withAllure;
 
-import io.qameta.allure.Attachment;
 import org.testng.annotations.Test;
 
 public class loginTest {
 
     @Test(priority = 1,description="Valid Login Scenario with wrong username and password.")
     public void positiveLoginTestCase(){
-        apiLibrary.addTextToReport("From Positive Login Testing...");
+        utilsLibrary.addTextToReport("From Positive Login Testing...");
     }
 
     @Test(priority = 2,description="Invalid Login Scenario with wrong username and password.")
     public void negativeLoginTestCase(){
-        apiLibrary.addTextToReport("From negative Login Test Case");
+        utilsLibrary.addTextToReport("From negative Login Test Case");
     }
 
 
-    
+
 }
